cf81e6eb7206e6231f7da3da2ce1ef085132f480
Miguel Molina <hi@mvader.me>
2015-10-08 11:24:19 +0200

    Minified build

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,11 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
     })
   ]
 };
